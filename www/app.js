4975af1c75ef76f18e678a69380d35414f529e1f
szymanskir <ryszard.szymanski@outlook.com>
2022-09-26 18:24:42 +0200

    chore: remove leftover console.log call

--- www/app.js
+++ www/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   const observer = new IntersectionObserver(function(entries) {
     if (entries[0].intersectionRatio > 0) {
-      console.log(Math.random());
       Shiny.setInputValue("screen_end_reached", true, { priority: "event" })
     }
   });
